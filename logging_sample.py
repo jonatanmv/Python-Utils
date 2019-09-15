74493b20cb310f4f28800e92b1b3bd835535a4c0
Jonatan Morales <jonatan.mv@gmail.com>
2019-09-15 11:52:18 +0200

    Update and rename logging.py to logging_sample.py

--- logging_sample.py
+++ logging_sample.py
@@ -1,31 +1,39 @@
 import logging
 import logging.handlers
+import os
+
+from pathlib import Path
 
 # Settings
 # Generic logging parameters
+LOG_TO_FILE = True
+LOG_TO_CONSOLE = True
 LOG_NAME = __name__
 LOG_DATE_FORMAT = '%Y-%m-%d %H:%m'
 # Logging config parameters for console
 LOG_FORMAT_CONSOLE = '%(asctime)s [%(levelname)5s] [%(name)s] [%(filename)s:%(lineno)d] %(message)s'
 LOG_LEVEL_CONSOLE = 'DEBUG'
 # Logging config parameters for file
-LOG_FILENAME = "logfile.log"
+FILANEME_WITHOUT_EXTENSION = Path(__file__).with_suffix('').stem
+LOG_FILENAME = FILANEME_WITHOUT_EXTENSION+"log"
 LOG_FORMAT_FILE = '%(asctime)s [%(levelname)5s] [%(name)s] [%(filename)s:%(lineno)d] %(message)s'
 LOG_LEVEL_FILE = 'DEBUG'LOG_LEVEL_FILE = logging.DEBUG
-
-# Create a console handler
-console_handler = logging.StreamHandler()
-console_handler.setLevel(LOG_LEVEL_CONSOLE)
-
-# Create a file handler
-file_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1048576, backupCount=2)
-file_handler.setLevel(LOG_LEVEL_FILE)
 
 # Set format to handlers
 formatter_console = logging.Formatter(fmt=LOG_FORMAT_CONSOLE,datefmt=LOG_DATE_FORMAT)
 formatter_file = logging.Formatter(fmt=LOG_FORMAT_FILE,datefmt=LOG_DATE_FORMAT)
-console_handler.setFormatter(formatter_console)
-file_handler.setFormatter(formatter_file	)
+
+# Create a console handler
+if LOG_TO_CONSOLE:
+  console_handler = logging.StreamHandler()
+  console_handler.setLevel(LOG_LEVEL_CONSOLE)
+  console_handler.setFormatter(formatter_console)
+  
+# Create a file handler
+if LOG_TO_FILE:
+  file_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1048576, backupCount=2)
+  file_handler.setLevel(LOG_LEVEL_FILE)
+  file_handler.setFormatter(formatter_file)
 
 # Create the logger and add handlers
 log = logging.getLogger(LOG_NAME)
